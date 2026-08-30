fc86314f644a501fef77686f99441c6b29c236f2
Maksim <maksim.minakov.83@mail.ru>
2026-08-30 12:30:07 +0300

    Initial Commit

---
+++ internship/internshipApp.swift
@@ -0,0 +1,17 @@
+//
+//  internshipApp.swift
+//  internship
+//
+//  Created by Максим Минаков on 30.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct internshipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
