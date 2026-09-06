a0e451f010881f5538f89a24da63ec2118683bba
Maksim <maksim.minakov.83@mail.ru>
2026-09-06 22:41:33 +0300

    git add App/internshipApp.swift Views/DoctorsListView.swift

--- internship/App/internshipApp.swift
+++ internship/App/internshipApp.swift
@@ -1,11 +1,15 @@
 import SwiftUI
 
+
 @main
 struct internshipApp: App {
     
+    @State private var viewModelDoctorsList = DoctorsListViewModel(dataService: BundleDataService())
+      
     var body: some Scene {
         WindowGroup {
             DoctorsListView()
+                .environment(viewModelDoctorsList)
         }
     }
 }
--- internship/Views/DoctorsListView.swift
+++ internship/Views/DoctorsListView.swift
@@ -1,8 +1,10 @@
 import SwiftUI
 
 struct DoctorsListView: View {
+    @Environment(DoctorsListViewModel.self) private var viewModelDoctorsList
+    
     
     var body: some View {
-       
+        
     }
 }
